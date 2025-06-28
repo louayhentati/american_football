639084f30b6c5eb4efbcee4c19ac11eab137f975
louayhentati <louay.hh7@gmail.com>
2025-06-28 02:47:45 +0200

    upd to include the home and away team

--- app/controllers/game.py
+++ app/controllers/game.py
@@ -1,12 +1,14 @@
 import csv
 from datetime import datetime
 import io
+
 from flask import Flask, render_template, request, redirect, url_for, flash, make_response, Response
 from flask_login import login_required
 from app.extensions import db
 from app.models.drive import DriveModel
 from app.models.game import GameModel
 from app.models.play import PlayModel
+from app.models.team import TeamModel
 
 
 class GameController:
@@ -22,46 +24,82 @@ class GameController:
         self.app.add_url_rule(rule='/game/<int:game_id>/add-drive', view_func=self.add_drive, methods=['POST'])
         self.app.add_url_rule(rule='/game/<int:game_id>/drive-chart', view_func=self.drive_chart)
         self.app.add_url_rule(rule='/game/<int:game_id>/export', view_func=self.export_game)
-        self.app.add_url_rule(rule='/game/<int:game_id>/drive/<int:drive_id>/play-chart', view_func=self.drive_play_chart)
+        self.app.add_url_rule(rule='/game/<int:game_id>/drive/<int:drive_id>/play-chart',
+                              view_func=self.drive_play_chart)
 
     @login_required
     def game_options(self) -> str:
         games = GameModel.query.all()
-        return render_template('game/game_options.html', games=games)
+        print(games)
+        return render_template(template_name_or_list='game/game_options.html', games=games)
 
     @login_required
     def game_detail(self, game_id: int) -> str:
-        game = GameModel.query.get_or_404(game_id)
-        return render_template('game/game_detail.html', game=game)
+        game = GameModel.get_by_id(game_id)
+        return render_template(template_name_or_list='game/game_detail.html', game=game)
 
     @login_required
     def add_game(self) -> str | Response:
         if request.method == 'POST':
             try:
+                data = request.form
+                game_name = data.get('game_name')
+                game_date = data.get('date')
+                game_time = data.get('time')
+                try:
+                    home_team_id = int(data.get('home_team_id'))
+                    away_team_id = int(data.get('away_team_id'))
+                except ValueError:
+                    flash(message='Please enter a valid team ID.', category='warning')
+                    return redirect(request.url)
+
+                if any([not game_name, not game_date, not game_time, not home_team_id, not away_team_id]):
+                    flash(message="Something weired happened. Please try again later.", category='danger')
+                    return redirect(request.url)
+
+                if home_team_id == away_team_id:
+                    flash(message="Home and Away teams must be different.", category="warning")
+                    return redirect(request.url)
+
+                home_check = TeamModel.query.get(home_team_id)
+                away_check = TeamModel.query.get(away_team_id)
+
+                if not home_check or not away_check:
+                    flash(message="An error occurred while adding a new game. Please try again.", category='danger')
+                    return redirect(request.url)
+
                 game = GameModel(
-                    game_name=request.form.get('game_name'),
-                    date=datetime.strptime(request.form.get('date'), '%Y-%m-%d'),
-                    time=request.form.get('time')
+                    name=game_name,
+                    game_date=datetime.strptime(game_date, "%Y-%m-%d"),
+                    game_time=game_time,
+                    home_team_id=home_team_id,
+                    away_team_id=away_team_id
                 )
+
                 db.session.add(game)
                 db.session.commit()
-                flash('Game added successfully!', 'success')
+                flash(message='Game added successfully!', category='success')
                 return redirect(url_for('game_options'))
             except Exception as e:
                 db.session.rollback()
-                flash(f'Error adding game: {str(e)}', 'error')
-        return render_template('game/add_game.html')
+                flash(message="An error occurred while adding a new game. Please try again.", category='danger')
+                print(f"[{type(e).__name__}] Failed to add game: {e}")
+
+        # GET Handler
+        teams = TeamModel.query.all()
+        return render_template(template_name_or_list='game/add_game.html', teams=teams)
 
     @login_required
     def delete_game(self, game_id: int) -> Response:
         try:
-            game = GameModel.query.get_or_404(game_id)
+            game = GameModel.get_by_id(game_id)
             db.session.delete(game)
             db.session.commit()
-            flash('Game deleted successfully', 'success')
+            flash(message='Game deleted successfully', category='success')
         except Exception as e:
             db.session.rollback()
-            flash(f'Error deleting game: {str(e)}', 'error')
+            flash(message="An error occurred while deleting the game. Please try again.", category='danger')
+            print(f"[{type(e).__name__}] Failed to delete game: {e}")
         return redirect(url_for('game_options'))
 
     @login_required
@@ -70,33 +108,35 @@ class GameController:
             drive = DriveModel(game_id=game_id)
             db.session.add(drive)
             db.session.commit()
-            flash('Drive added successfully!', 'success')
+            flash(message='Drive added successfully!', category='success')
         except Exception as e:
             db.session.rollback()
-            flash(f'Error adding drive: {str(e)}', 'error')
-        return redirect(url_for('game_detail', game_id=game_id))
+            flash(message="An error occurred while adding the drive. Please try again.", category='danger')
+            print(f"[{type(e).__name__}] Failed to add drive: {e}")
+        return redirect(url_for(endpoint='game_detail', game_id=game_id))
 
+    @staticmethod
+    def convert(yard_line: int) -> int:
+        """converts yard-field values to format 0-100"""
+        if yard_line == 50:
+            return 50
+        if yard_line < 0:
+            return -yard_line
+        return 100 - yard_line
 
-    #Function for converting yard-field values to format 0-100
-    def convert(self,yl: int) -> float:
-        if yl <  0: return -yl
-        if yl == 50: return 50
-        # yl>0
-        return 100 - yl 
-
-    @login_required 
+    @login_required
     def drive_play_chart(self, game_id, drive_id):
-        game = GameModel.query.get_or_404(game_id)
-        drive = DriveModel.query.filter_by(game_id=game_id,id = drive_id).first_or_404()
+        game = GameModel.get_by_id(game_id)
+        drive = DriveModel.query.filter_by(game_id=game_id, id=drive_id).first_or_404()
         plays = PlayModel.query.filter_by(drive_id=drive.id).order_by(PlayModel.id).all()
-        
+
         plays_data = []
         for play in plays:
 
             raw_start = play.yard_line
-            raw_end   = play.yard_line 
+            raw_end = play.yard_line
             start_yard = self.convert(raw_start)
-            end_yard = self.convert(raw_end)+play.gain_loss
+            end_yard = self.convert(raw_end) + play.gain_loss
 
             start_yard = max(0, min(100, start_yard))
             end_yard = max(0, min(100, end_yard))
@@ -117,12 +157,17 @@ class GameController:
                 'loss': loss_detected
             })
         print(f"Drives Data: {plays_data}")
-        return render_template('drive/drive_play_chart.html', game=game, drive=drive, plays=plays_data, max=max)
-    
+        return render_template(
+            template_name_or_list='drive/drive_play_chart.html',
+            game=game,
+            drive=drive,
+            plays=plays_data,
+            max=max
+        )
 
     @login_required
     def drive_chart(self, game_id):
-        game = GameModel.query.get_or_404(game_id)
+        game = GameModel.get_by_id(game_id)
         drives = DriveModel.query.filter_by(game_id=game_id).all()
 
         drives_data = []
@@ -130,12 +175,13 @@ class GameController:
             plays = PlayModel.query.filter_by(drive_id=drive.id).order_by(PlayModel.id).all()
             number_plays = len(plays)
 
-            if number_plays == 0 or not plays:continue #if no play in a drive, continue with the next 
+            if number_plays == 0 or not plays:
+                continue  # if no play in a drive, continue with the next
 
             raw_start = plays[0].yard_line
-            raw_end   = plays[-1].yard_line 
+            raw_end = plays[-1].yard_line
             start_yard = self.convert(raw_start)
-            end_yard = self.convert(raw_end)+plays[-1].gain_loss
+            end_yard = self.convert(raw_end) + plays[-1].gain_loss
 
             if drive.result and drive.result.lower() == 'touchdown': end_yard = 100
 
@@ -150,20 +196,23 @@ class GameController:
 
             drives_data.append({
                 'id': drive.id,
-                'team': game.game_name.split(' vs ')[0],
+                'team': game.name.split(' vs ')[0],
                 'start': start_yard,
                 'end': end_yard,
                 'result': drive.result or 'Unknown',
                 'play_count': len(plays),
                 'loss': loss_detected
             })
-        print(f"Drives Data: {drives_data}")
-        return render_template('drive/drive_chart.html', game=game, drives=drives_data, max=max)
-
+        return render_template(
+            template_name_or_list='drive/drive_chart.html',
+            game=game,
+            drives=drives_data,
+            max=max
+        )
 
     @login_required
     def export_game(self, game_id):
-        game = GameModel.query.get_or_404(game_id)
+        game = GameModel.get_by_id(game_id)
         drives = DriveModel.query.filter_by(game_id=game.id).order_by(DriveModel.id).all()
 
         csv_data = io.StringIO()
